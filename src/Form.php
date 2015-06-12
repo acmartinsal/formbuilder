d86dc209b1d088c35b46d6b4602011acfd13024a
root <root@pve.internal.martins-almeida.fr>
2015-06-12 19:37:16 +0200

    added files

---
+++ src/Form.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Noisettine\Tools;
+
+/**
+ * Description of Form
+ *
+ * @author S
+ */
+class Form extends \Micro\Form {
+
+    protected function render_field() {
+        $html = "\n";
+        $html .= '<div class="form-group">';
+        
+        if (!$this->attributes) {
+            $this->attributes = array();
+        }
+
+        // Configure the attributes
+        if ($this->type != 'checkbox') {
+            $this->attributes = $this->attributes + array('class' => 'form-control');
+        }
+
+        // Get the current value
+        if ($this->value !== NULL) {
+            $value = $this->value;
+        } else {
+            $value = $this->validation->value($this->field);
+        }
+
+        if ($this->label && $this->type != 'checkbox') {
+            //$html .= '<div class="col-sm-6">';
+            $html .= '<label class="col-sm-3 control-label" for="' . $this->field . '">' . $this->label . ' ' .(isset($this->attributes['required']) ? '*': '').'</label><div class="col-sm-9">';
+        }else{
+            $html .= '<div class="col-sm-offset-3 col-sm-9"><div class="checkbox"><label>';
+        }
+        
+        if ($this->type == 'select') {
+            $html .= \Micro\HTML::select($this->field, $this->options, $value, $this->attributes);
+        } elseif ($this->type == 'textarea') {
+            $html .= \Micro\HTML::tag('textarea', $value, $this->attributes);
+        } else {
+            
+            // Input field
+            $this->attributes = $this->attributes  + array('type' => $this->type, 'value' => $value);
+
+            $html .= \Micro\HTML::tag('input', FALSE, $this->attributes);
+            if ($this->type == 'checkbox') {
+                $html .= " ".$this->label;
+                $html .= '</label></div>';
+            }
+        }
+        $html .= '</div>';
+        $html .= '</div>';
+        
+        // If there was a validation error
+        if ($error = $this->validation->error($this->field)) {
+            if (isset($this->attributes['class'])) {
+                $this->attributes['class'] .= ' error';
+            } else {
+                $this->attributes['class'] = $this->field . ' ' . $this->type . ' error';
+            }
+
+            $html .= "\n<div class=\"error_message\">$error</div>";
+        }
+
+        if ($this->tag) {
+            $html = \Micro\HTML::tag($this->tag, $html . "\n") . "\n";
+        }
+
+        return $html;
+    }
+    
+    public function required($required)
+    {
+        if(false !== $required)
+        {
+            $this->attributes(array('required' => 'required'));
+        }
+    }
+}
+
---
+++ src/FormBuilder.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Noisettine\Tools;
+
+/**
+ * Description of FormBuilder
+ *
+ * @author Sylvain
+ */
+use Micro\Validation;
+
+class FormBuilder
+{
+
+    private $formfields = array();
+
+    public function __construct($oData)
+    {
+        foreach ($oData->children as $key => $val) {
+            // Cet élément de formulaire a t il des enfants ?
+            if (count($oData->children->$key->children) == 0) {
+                // Non
+                $this->addFormField($key, $oData->children->$key);
+            } else {
+                // Oui
+                self::__construct($oData->children->$key);
+            }
+        }
+    }
+
+    public function addFormField($field_name, $field_details)
+    {
+        $this->formfields[$field_name] = $field_details;
+    }
+
+    public function build()
+    {
+        $form = new Form(new Validation(null));
+        foreach ($this->formfields as $form_field) {
+            if ("form" === $form_field->vars->block_prefixes[0]) {
+                $titre = (!empty($form_field->vars->label) ? $form_field->vars->label : "");
+                if ($form_field->vars->block_prefixes[1] == "text") { // Champs texte
+                    if ($form_field->vars->block_prefixes[2] == "textarea") { // textarea
+                        $form->{$form_field->vars->name}->textarea()->label($titre)->value($form_field->vars->value)->attributes((array) $form_field->vars->attr);
+                    } else if ($form_field->vars->block_prefixes[2] == "captcha") {						
+                    } else if ($form_field->vars->block_prefixes[2] == "password") {
+                         $form->{$form_field->vars->name}->input($form_field->vars->block_prefixes[2])->label($titre)->value($form_field->vars->value)->attributes((array) $form_field->vars->attr)->required($form_field->vars->required);
+                    } else {
+                        if (!isset($form_field->vars->attr)) {
+                            $form_field->vars->attr = array();
+                        }
+                        $form->{$form_field->vars->name}->input($form_field->vars->block_prefixes[1])->label($titre)->value($form_field->vars->value)->attributes((array) $form_field->vars->attr)->required($form_field->vars->required);
+                    }
+				}elseif($form_field->vars->block_prefixes[1] == "number") { // Nombre
+					$form->{$form_field->vars->name}->input("text")->label($titre)->value($form_field->vars->value)->attributes((array) $form_field->vars->attr)->required($form_field->vars->required);
+                } elseif ($form_field->vars->block_prefixes[1] == "time") { // Heure
+                    $heure = date('H:i');
+                    $form->{$form_field->vars->name}->input('text')->label($titre)->value($heure)->attributes((array) $form_field->vars->attr)->required($form_field->vars->required);  
+                } elseif ($form_field->vars->block_prefixes[1] == "date") { // Date
+                    //$date = date('d/m/Y');
+                    $form->{$form_field->vars->name}->input('text')->label($titre)->value($form_field->vars->value)->attributes((array) $form_field->vars->attr)->required($form_field->vars->required);  
+                
+                } elseif ($form_field->vars->block_prefixes[1] == "file") { // File
+                    $form->{$form_field->vars->name}->input($form_field->vars->block_prefixes[1])->value('')->label($titre)->wrap('p')->required($form_field->vars->required);
+                } elseif ($form_field->vars->block_prefixes[1] == "checkbox") { // Checkbox
+                    if(true == $form_field->vars->checked){
+                        $form_field->vars->attr = array_merge(array('checked' => 'checked'),$form_field->vars->attr); 
+                    }
+                    $form->{$form_field->vars->name}->input($form_field->vars->block_prefixes[1])->value('1')->label($titre)->attributes((array) $form_field->vars->attr)->required($form_field->vars->required);
+                } elseif ($form_field->vars->block_prefixes[1] == "choice") { // Selects or Radios
+                    $choices = array();
+                    foreach ($form_field->vars->choices as $choice) {
+                        if (true === is_object($choice->data)) { // Entity
+                            $choices[$choice->value] = $choice->label;
+                        } else {
+                            $choices[$choice->value] = $choice->label;
+                        }
+                    }
+                    $form->{$form_field->vars->name}->select($choices)->label($titre)->value($form_field->vars->value)->required($form_field->vars->required);
+                } else {
+                    foreach ($form_field->vars as $var) {
+                        //var_dump($var);
+                    }
+                }
+            }elseif("repeated" == $form_field->vars->block_prefixes[0]){
+                foreach($form_field->children as $children){
+                    $titre = (!empty($children->vars->label) ? $children->vars->label : "");
+                    $form->{$children->vars->name}->input('password')->label($titre)->wrap('p')->required($form_field->vars->required);
+                }
+            }elseif("collection" == $form_field->vars->block_prefixes[1]){
+                foreach($form_field->vars->prototype->children as $children){
+                    $titre = (!empty($children->vars->label) ? $children->vars->label : "");
+                    $form->{$children->vars->name}->input('text')->label($titre)->wrap('p')->required($form_field->vars->required);
+                }
+            }else{  
+                foreach($form_field->children as $children){
+                    $titre = (!empty($children->vars->label) ? $children->vars->label : "");
+                    $form->{$children->vars->name}->input('text')->label($titre)->wrap('p')->required($form_field->vars->required);
+                }
+            }
+        }
+        return $form;
+    }
+
+}
+
+// End2
